bf2c3dceac57d98d14329f363749a8130827c092
Kracie <joamo950@student.liu.se>
2014-12-02 18:32:55 +0100

    Körbar input_handler

--- input_handler.cpp
+++ input_handler.cpp
@@ -1,7 +1,7 @@
 #include "input_handler.h"
 
-void input_handler::update(/*class gamefield& field*/)
-{/*
+void input_handler::update(class gamefield& field)
+{
     while(SDL_PollEvent(&event))
     {
         switch(event.type)
@@ -10,57 +10,58 @@ void input_handler::update(/*class gamefield& field*/)
                 switch(event.key.keysym.sym)
                 {
                     case SDLK_a:
-                        field.character_vector[1].left_key = 1;
+                        field.character_vector[0].left_key = 1;
                         break;
                     case SDLK_d:
-                        field.character_vector[1].right_key = 1;
+                        field.character_vector[0].right_key = 1;
                         break;
                     case SDLK_w:
-                        field.character_vector[1].up_key = 1;
+                        field.character_vector[0].up_key = 1;
                         break;
                     case SDLK_s:
-                        field.character_vector[1].down_key = 1;
+                        field.character_vector[0].down_key = 1;
                         break;
                     case SDLK_y:
-                        field.character_vector[1].turn_left_key = 1;
+                        field.character_vector[0].turn_left_key = 1;
                         break;
                     case SDLK_u:
-                        field.character_vector[1].turn_right_key = 1;
+                        field.character_vector[0].turn_right_key = 1;
                         break;
                     case SDLK_i:
-                        field.character_vector[1].shoot_key = true;
+                        field.character_vector[0].shoot_key = true;
                         break;
                     case SDLK_o:
-                        field.character_vector[1].ultimate();
+                        field.character_vector[0].ultimate();
                         break;
                 }
             case SDL_KEYUP:
                 switch(event.key.keysym.sym)
             {
                 case SDLK_a:
-                    field.character_vector[1].left_key = 0;
+                    field.character_vector[0].left_key = 0;
                     break;
                 case SDLK_d:
-                    field.character_vector[1].right_key = 0;
+                    field.character_vector[0].right_key = 0;
                     break;
                 case SDLK_w:
-                    field.character_vector[1].up_key = 0;
+                    field.character_vector[0].up_key = 0;
                     break;
                 case SDLK_s:
-                    field.character_vector[1].down_key = 0;
+                    field.character_vector[0].down_key = 0;
                     break;
                 case SDLK_y:
-                    field.character_vector[1].turn_left_key = 0;
+                    field.character_vector[0].turn_left_key = 0;
                     break;
                 case SDLK_u:
-                    field.character_vector[1].turn_right_key = 0;
+                    field.character_vector[0].turn_right_key = 0;
                     break;
                 case SDLK_i:
-                    field.character_vector[1].shoot_key = false;
+                    field.character_vector[0].shoot_key = false;
                     break;
-                case SDLK_o:
-                    field.charachter_vector[1].stopultimate;
+                /*case SDLK_o:
+                    field.character_vector[1].stopultimate;
                     break;
+                    */
             }
 
 
@@ -68,6 +69,6 @@ void input_handler::update(/*class gamefield& field*/)
 
 
 
-    }*/
+    }
 return;
 }
